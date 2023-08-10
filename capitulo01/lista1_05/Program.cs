24fdd957e6c5b43611b1c1693b7def316f84660a
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-08-10 15:14:30 -0300

    Add files via upload

---
+++ capitulo01/lista1_05/Program.cs
@@ -0,0 +1,36 @@
+﻿// Elabore um programa em C# que declare um vetor de strings com nomes predefinidos. Solicite ao usuário um nome para busca. Verifique se o nome está presente no vetor e exiba uma mensagem informando se o nome foi encontrado ou não.
+
+using System;
+
+namespace lista1_05
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            string[] vetor = { "Carol", "Gabi", "Fefe", "Jao", "Tete" };
+            Console.Write("\nDigite um nome para buscar dentro do vetor: ");
+            string nomeUsuario = Console.ReadLine();
+            bool encontrado = false;
+
+            foreach (string nome in vetor)
+            {
+                if (nome.Equals(nomeUsuario))
+                {
+                    encontrado = true;
+                    break;
+                }
+            }
+
+            if (encontrado)
+            {
+                Console.WriteLine($"O nome {nomeUsuario} foi encontrado.");
+                    
+            }
+            else
+            {
+                Console.WriteLine($"O nome {nomeUsuario} não foi encontrado.");
+            }
+        }
+    }
+}
