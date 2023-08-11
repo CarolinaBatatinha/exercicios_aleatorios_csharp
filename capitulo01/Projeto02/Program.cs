9c3cf8a1c829a0c571e322922a56342f5f2b4b1d
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-08-11 17:19:58 -0300

    Add files via upload

---
+++ capitulo01/Projeto02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string frase = "A velha debaixo da cama";
+        char[] vetor = frase.ToCharArray();
+        Array.Reverse(vetor);
+
+        foreach (char letra in vetor)
+        {
+            Console.Write(letra);
+        }
+    }
+}
