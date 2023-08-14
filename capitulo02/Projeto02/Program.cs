28b34f417434bd3a449c9869286a37dddf053df4
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-08-14 14:55:28 -0300

    Add files via upload

---
+++ capitulo02/Projeto02/Program.cs
@@ -0,0 +1,17 @@
+﻿// como representar uma matriz:
+double[,] notas =
+{
+    { 8.7, 5.4, 6.2 },
+    { 3.8, 2.7, 8.5 },
+    { 9.1, 0.9, 4.2 },
+    { 7.1, 1.8, 8.8 }
+};
+Console.WriteLine(notas[0,1]);
+
+notas[0, 1] = 6.4;
+
+// como declarar uma matriz vazia:
+
+    //double[,] notas = new double[4,3];
+
+
