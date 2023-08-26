8724943674b970b9f33102e5ef4ccbf32da030d4
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-08-26 15:47:07 -0300

    Add files via upload

---
+++ exercicios_avulsos/Exercicio06/Program.cs
@@ -0,0 +1,27 @@
+﻿// Declare um vetor de 10 posições e o preencha com os 10 primeiros números impares e o escreva.
+
+using System;
+
+namespace Exercicio06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[10];
+            int numeroImpar = 1;
+            
+            for (int i = 0; i < 10; i++)
+            {
+                vetor[i] = numeroImpar;
+                numeroImpar += 2;
+            }
+            Console.WriteLine("Vetor de números ímpares: ");
+
+            foreach (int numero in vetor)
+            {
+                Console.Write(numero + " ");
+            }
+        }
+    }
+}
