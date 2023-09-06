49f112a508897ee1a17fa804c47aa2f64d2243ea
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-09-06 07:52:00 -0300

    Add files via upload

---
+++ ex04/Program.cs
@@ -0,0 +1,24 @@
+﻿//Crie um programa em C# que declare um vetor de inteiros com valores predefinidos. Contabilize e imprima a quantidade de números pares e ímpares presentes no vetor.
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] vetor = { 12, 43, 26, 18, 31 };
+        int pares = 0;
+        int impares = 0;
+
+        foreach (int numero in vetor)
+        {
+            if (numero % 2 == 0)
+            {
+                pares++;
+            }
+            else
+            {
+                impares++;
+            }
+        }
+        Console.WriteLine($"São {pares} números pares e {impares} números ímpares.");
+    }
+}
