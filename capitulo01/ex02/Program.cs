33cfec0ce38e4bae6581a0c1433e5e9cfb9df7c0
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-09-06 07:50:44 -0300

    Add files via upload

---
+++ capitulo01/ex02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Escreva um programa em C# que declare um vetor de inteiros com valores predefinidos. Calcule a média dos valores contidos no vetor e imprima o resultado na tela.
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] vetor1 = { 1, 2, 3, 4, 5 };
+        int soma = 0;
+
+        foreach (int numero in vetor1)
+        {
+            soma+=numero;
+        }
+
+        double media = soma / vetor1.Length;
+
+        Console.WriteLine($"A média dos valores contidos no vetor é igual a {media}");
+    }
+}
