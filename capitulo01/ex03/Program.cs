8dd7d9d6c899416065509d4d1e9c9882a48cda97
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-09-06 07:51:33 -0300

    Add files via upload

---
+++ capitulo01/ex03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Desenvolva um programa em C# que declare um vetor de inteiros com valores predefinidos. Encontre e imprima o maior valor presente no vetor.
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] vetor = { 1, 12, 3, 4 };
+        int maiorNumero = 0;
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i]>maiorNumero)
+            {
+                maiorNumero = vetor[i];
+            }
+        }
+        Console.WriteLine($"O maior número é {maiorNumero}.");
+    }
+}
