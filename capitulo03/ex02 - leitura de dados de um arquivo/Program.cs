3e926440638538b48048ecfece3a2823c8dd68d6
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-09-11 09:26:02 -0300

    Add files via upload

---
+++ capitulo03/ex02 - leitura de dados de um arquivo/Program.cs
@@ -0,0 +1,11 @@
+﻿string caminhoArquivo = "D:\\SENAC\\2 - periodo2\\Algoritmos e Programação II\\exercícios\\capitulo03 - manipulacao de arquivos\\ex02\\arquivo.txt";
+
+using (StreamReader leitor = new StreamReader(caminhoArquivo))
+{
+    //aqui os dados do arquivo são lidos usando o objeto 'leitor'
+    string linha;
+    while ((linha = leitor.ReadLine()) != null)
+    {
+        Console.WriteLine(linha); // exibe a linha lida
+    }
+}
