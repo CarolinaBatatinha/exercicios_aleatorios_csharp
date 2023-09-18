e0f994624fcf82e24d1db823aab55d04f8853025
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-09-18 08:58:15 -0300

    Add files via upload

---
+++ capitulo06 - algoritmos recursivos/ex02 - aula univesp/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    public static int Fat(int numero)
+    {
+        if (numero == 0) // por definição, o 0! é igual a 1
+        {
+            return 1;
+        }
+        else
+        {
+            int resultado = numero * Fat(numero - 1);
+            return resultado;
+        }
+    }
+
+    public static void Main(String[] args)
+    {
+        int res = Fat(4);
+        Console.WriteLine(res);
+    }
+}
