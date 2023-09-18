da5544761d337f0deda1bb4c4657daa17a2c17bd
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-09-18 08:59:29 -0300

    Add files via upload

---
+++ capitulo06 - algoritmos recursivos/ex04/Program.cs
@@ -0,0 +1,5 @@
+﻿// Implementar uma função recursiva para calcular o n-ésimo termo da sequência de Fibonacci.
+// Considere os três pontos definidos para o problema:
+// 1) f(0) = 0, f(1 ) = 1, f(n) = f(n-1) + f(n-2) para n = 2 
+// 2) n = 0 ou n = 1
+// 3) n deve ser decrementado a cada chamada
