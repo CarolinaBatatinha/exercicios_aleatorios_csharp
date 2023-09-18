b0cb14d5693e8d9babdb5bad62edc687f0832a9c
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-09-18 08:59:00 -0300

    Add files via upload

---
+++ capitulo06 - algoritmos recursivos/ex03/Program.cs
@@ -0,0 +1,26 @@
+﻿// Implementar uma função recursiva em C# para calcular o fatorial de um número inteiro positivo passado como parâmetro.
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número inteiro positivo: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        int resultado = Fatoriale(numero);
+        Console.WriteLine($"O resultado do fatorial de {numero} é igual a {resultado}");
+    }
+
+    public static int Fatoriale(int numero)
+    {
+        if (numero == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            int rec = numero * Fatoriale(numero - 1);
+            return rec;
+        }
+    }
+}
