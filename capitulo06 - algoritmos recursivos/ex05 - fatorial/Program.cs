c03cf980c9c27ff1a8bfa0b2026181e5867fbaca
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-09-18 09:00:06 -0300

    Add files via upload

---
+++ capitulo06 - algoritmos recursivos/ex05 - fatorial/Program.cs
@@ -0,0 +1,27 @@
+﻿// FATORIAL
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número inteiro para calcular o fatorial:");
+        int n = int.Parse(Console.ReadLine());
+
+        long resultado = Fatorial(n);
+
+        Console.WriteLine($"O fatorial de {n} é {resultado}.");
+    }
+    static long Fatorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Fatorial(n - 1);
+        }
+    }
+}
